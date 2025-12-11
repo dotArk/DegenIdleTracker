d1de7fd280583bb75921f2018a9d6bed04a842d8
dotArk <barrylunsford444@gmail.com>
2025-12-11 09:18:55 -0500

    Update api.js

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ const DegenAPI = (function() {
     
     const mod = {};
     let t = null;
+    const API_ROOT = "https://api.degenidle.com/api/";
     
     mod.init = function(tracker) {
         t = tracker;
@@ -10,54 +11,63 @@ const DegenAPI = (function() {
     };
     
     function setup() {
-        t.log('Setting up XHR interception...');
+        t.log('Setting up API interception (XP Tracker pattern)...');
         
-        const origXHR = window.XMLHttpRequest;
-        window.XMLHttpRequest = function() {
-            const xhr = new origXHR();
-            const origOpen = xhr.open;
-            const origSend = xhr.send;
+        // 1. Hook fetch (correct pattern)
+        const origFetch = window.fetch;
+        window.fetch = async function(input, init) {
+            const response = await origFetch.apply(this, arguments);
             
-            xhr.open = function(method, url, async, user, password) {
-                this._method = method;
-                this._url = url;
-                t.log(`XHR open: ${method} ${url}`);
-                return origOpen.call(this, method, url, async || true, user, password);
-            };
-            
-            xhr.send = function(body) {
-                if (this._url && this._url.includes('api.degenidle.com')) {
-                    t.log(`XHR send to: ${this._url}`);
-                    
-                    const origOnReadyStateChange = this.onreadystatechange;
-                    const origOnLoad = this.onload;
-                    
-                    this.addEventListener('load', function() {
-                        if (this.status >= 200 && this.status < 300) {
-                            try {
-                                if (this.responseText) {
-                                    const data = JSON.parse(this.responseText);
-                                    t.log(`XHR response from: ${this._url}`);
-                                    if (t.data && t.data.process) {
-                                        t.data.process(this._url, data);
-                                    }
-                                }
-                            } catch(e) {}
-                        }
-                    });
-                    
-                    this.addEventListener('error', function() {
-                        t.log(`XHR error: ${this._url}`);
-                    });
-                }
+            try {
+                const url = (typeof input === 'string') ? input : (input?.url || '');
                 
-                return origSend.call(this, body);
-            };
+                if (url.startsWith(API_ROOT)) {
+                    const clone = response.clone();
+                    clone.json()
+                        .then(json => {
+                            t.log(`Fetch response: ${url}`);
+                            if (t.data && t.data.process) {
+                                t.data.process(url, json);
+                            }
+                        })
+                        .catch(() => {});
+                }
+            } catch(e) {}
             
-            return xhr;
+            return response;
         };
         
-        t.log('XHR interception ready');
+        // 2. Hook XMLHttpRequest (XP Tracker pattern)
+        (function() {
+            const XHR = XMLHttpRequest;
+            
+            function newXHR() {
+                const realXHR = new XHR();
+                
+                realXHR.addEventListener('readystatechange', function() {
+                    try {
+                        if (realXHR.readyState === 4 && realXHR.responseURL?.startsWith(API_ROOT)) {
+                            try {
+                                const json = JSON.parse(realXHR.responseText);
+                                t.log(`XHR response: ${realXHR.responseURL}`);
+                                if (t.data && t.data.process) {
+                                    t.data.process(realXHR.responseURL, json);
+                                }
+                            } catch(e) {
+                                // Not JSON
+                            }
+                        }
+                    } catch(e) {}
+                }, false);
+                
+                return realXHR;
+            }
+            
+            newXHR.prototype = XHR.prototype;
+            window.XMLHttpRequest = newXHR;
+        })();
+        
+        t.log('API interception ready');
     }
     
     mod.test = function() {
