5982ad868f45b73306618c25f7e6b74a2e569875
dotArk <barrylunsford444@gmail.com>
2025-12-11 09:58:21 -0500

    Update api.js

--- api.js
+++ api.js
@@ -7,23 +7,38 @@ const DegenAPI = (function() {
     
     m.init = function(tracker) {
         t = tracker;
-        setup();
-        t.log('API ready');
+        console.log('🔧 API init - tracker:', !!t, 't.data:', !!(t && t.data));
+        
+        setTimeout(() => {
+            console.log('🔧 Delayed setup - t.data:', !!(t && t.data));
+            setup();
+        }, 100);
     };
     
+    function proc(u, d) {
+        console.log('🔧 Proc called - u:', u, 't:', !!t, 't.data:', !!(t && t.data));
+        if (t && t.data && typeof t.data.process === 'function') {
+            console.log('✅ Calling t.data.process');
+            t.data.process(u, d);
+        } else {
+            console.log('❌ Cannot process: t.data.process not available');
+        }
+    }
+    
     function setup() {
+        console.log('🔧 Setting up intercept...');
+        
         const of = window.fetch;
         window.fetch = async function(i, init) {
             const r = await of.apply(this, arguments);
             const u = (typeof i === 'string') ? i : (i?.url || '');
             
             if (u && u.includes('api.degenidle.com')) {
+                console.log('🌐 Fetch:', u);
                 const c = r.clone();
                 try {
                     const d = await c.json();
-                    if (d && t.data && t.data.process) {
-                        t.data.process(u, d);
-                    }
+                    proc(u, d);
                 } catch(e) {}
             }
             return r;
@@ -41,13 +56,12 @@ const DegenAPI = (function() {
             const oS = x.send;
             x.send = function(b) {
                 if (this._u && this._u.includes('api.degenidle.com')) {
+                    console.log('📤 XHR:', this._u);
                     this.addEventListener('load', function() {
                         if (this.status >= 200 && this.status < 300 && this.responseText) {
                             try {
                                 const d = JSON.parse(this.responseText);
-                                if (d && t.data && t.data.process) {
-                                    t.data.process(this._u, d);
-                                }
+                                proc(this._u, d);
                             } catch(e) {}
                         }
                     }, false);
@@ -58,15 +72,9 @@ const DegenAPI = (function() {
         }
         nX.prototype = oX.prototype;
         window.XMLHttpRequest = nX;
+        
+        console.log('✅ Intercept ready');
     }
-    
-    m.test = function() {
-        fetch('https://api.degenidle.com/api/heartbeat/status')
-            .then(r => r.json())
-            .then(d => t.log('Test OK:', d.success))
-            .catch(e => t.log('Test fail:', e));
-        return true;
-    };
     
     window.DegenAPI = m;
     return m;
